0bb7d5d5a82916fdf05e8cd063be499709363721
Haneul Lee <lovesky4294@gmail.com>
2019-06-23 17:35:36 +0900

    preload images

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetAsync}
+          onFinish={this._handleFinishLoading}
+          onError={this._handleLoadingError}
+        />
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+  _loadAssetAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/instagram.png"),
+        require("./assets/images/icon.png"),
+        require("./assets/images/splash.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+  _handleFinishLoading = () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;

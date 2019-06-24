6728c508226bf567a0aca20772737993d9e67dbd
Haneul Lee <lovesky4294@gmail.com>
2019-06-24 23:17:31 +0900

    login component style & state

--- screens/LogInScreen/container.js
+++ screens/LogInScreen/container.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
+import { Alert } from "react-native";
 import LogInScreen from "./presenter";
 
 class Container extends Component {
+  state = {
+    username: "",
+    password: "",
+    isSubmitting: false
+  };
   render() {
-    return <LogInScreen />;
+    return (
+      <LogInScreen
+        {...this.state}
+        onChangeUsername={this._onChangeUsername}
+        onChangePassword={this._onChangePassword}
+        onSubmit={this._onSubmit}
+      />
+    );
   }
+  _onChangeUsername = text => {
+    this.setState({
+      username: text
+    });
+  };
+
+  _onChangePassword = text => {
+    this.setState({
+      password: text
+    });
+  };
+
+  _onSubmit = event => {
+    const { username, password, isSubmitting } = this.state;
+
+    if (!isSubmitting) {
+      if (username && password) {
+        this.setState({
+          isSubmitting: true
+        });
+      } else {
+        Alert.alert("all fields are required.");
+      }
+    }
+  };
 }
 export default Container;
